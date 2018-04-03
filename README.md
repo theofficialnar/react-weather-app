64625b76f78af51d1ea49a3def128c38241b3d0a
Nar Cuenca <narc.ph@gmail.com>
2018-04-03 16:27:53 +0800

    final project upload:

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple weather app via Openweather API
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h3 className="title-container__subtitle">Find out temeperature, conditions and more!</h3>
+  </div>
+)
+
+export default Titles;
